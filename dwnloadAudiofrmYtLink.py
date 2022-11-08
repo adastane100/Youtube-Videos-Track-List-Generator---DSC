fbfe81883d1b75cd96d082522c2bab0301a3aa6c
Kai Drumm <kaidrumm@protonmail.com>
2022-11-07 19:40:39 -0700

    Name files mp4 on download

--- dwnloadAudiofrmYtLink.py
+++ dwnloadAudiofrmYtLink.py
@@ -13,7 +13,7 @@ def dwnloadAudiofrmYtLink():
     out_file = video.download(output_path=destination)
 
     base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
+    new_file = base + '.mp4'
     os.rename(out_file, new_file)
 
     #print(yt.title + " has been successfully downloaded.")
