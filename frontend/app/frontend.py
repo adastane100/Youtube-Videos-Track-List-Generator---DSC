5d6eb09f9ef64c03f2cc68e8adb9ca36074d0ad7
Kai Drumm <kaidrumm@protonmail.com>
2022-12-05 21:18:40 -0700

    Update pip requirements

--- frontend/app/frontend.py
+++ frontend/app/frontend.py
@@ -9,8 +9,6 @@ import os, sys, platform
 import uuid
 from redis import Redis
 from minio import Minio
-from ShazamAPI import Shazam
-import AudioSegmenting, RecognizeTrack
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -62,30 +60,6 @@ def generate_tracks():
 
     request_id = uuid.uuid4()
     redisClient.lpush('to-downloader', f"{request_id}:{input_val}")
-
-    # download mp4 file from youtube link
-    # video_title = DownloadFromYoutube.downloadAudio(input_val)
-    # print('video_title :::: '+video_title)
-
-    # TODO: Send message to segmenter
-    # segmenting the downloaded file
-    # segmented_tracks = AudioSegmenting.segmentAudio(video_title[:-4])
-
-    # TODO: Watch for recognized tracks and display them
-    # all_track_titles = []
-    # if len(segmented_tracks) > 0:
-    #     print('in for loop of tracks')
-    #     for track in segmented_tracks:
-    #         track_info = {}
-    #         # identify tracks from mp4 segment file
-    #         track_info = RecognizeTrack.identify_track(video_title[:-4],track)
-    #         if track_info is not None and len(track_info) > 0 and track_info['title'] not in all_track_titles:
-    #             all_track_titles.append(track_info['title'])
-    #             identified_tracks.append(track_info)
-    
-    #     print('Total identified tracks :::'+str(len(identified_tracks)))
-    #     for single_track in identified_tracks:
-    #         print(single_track)
 
     # Wait for done message
     # TODO: Dynamically update page as results are found
