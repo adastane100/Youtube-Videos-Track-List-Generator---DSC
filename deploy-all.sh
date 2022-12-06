6f262f3ea6d3ddfb94730d2ace3cc1c1fdfef431
Kai Drumm <kaidrumm@protonmail.com>
2022-12-05 20:59:37 -0700

    Downloading service

--- deploy-all.sh
+++ deploy-all.sh
@@ -2,8 +2,8 @@ kubectl apply -f frontend/frontend-deployment.yaml
 kubectl apply -f frontend/frontend-ingress.yaml
 kubectl apply -f frontend/frontend-service.yaml
 
+kubectl apply -f downloading-service/downloading-deployment.yaml
 kubectl apply -f recognition-service/recognition-deployment.yaml
-
 kubectl apply -f segmenting-service/segmentation-deployment.yaml
 
 kubectl apply -f redis/redis-deployment.yaml
