ada883646fcb81260727c38551927e76c131044b
Kai Drumm <kaidrumm@protonmail.com>
2022-12-05 21:53:48 -0700

    Working on deployments

--- deploy-all.sh
+++ deploy-all.sh
@@ -8,3 +8,5 @@ kubectl apply -f segmenting-service/segmentation-deployment.yaml
 
 kubectl apply -f redis/redis-deployment.yaml
 kubectl apply -f redis/redis-service.yaml
+
+kubectl apply -f minio/minio-external-service.yaml
